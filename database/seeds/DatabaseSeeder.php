cc6919a9d774cb85203a519f6075d33686829766
Andrew Haswell <andrew.haswell@visualsoft.co.uk>
2017-01-26 10:14:49 +0000

    Populating DB Seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,13 +4,25 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-    }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $user = new App\User;
+    $user->name = 'Andrew Haswell';
+    $user->email = 'andrew.haswell@imperialoutpost.co.uk';
+    $user->password = bcrypt('bob');
+    $user->save();
+
+    factory(App\Models\account::class, 12)->create()->each(function ($v) {
+      factory(App\Models\balance::class, rand(15, 45))->create(['account_id' => $v->id]);
+      factory(App\Models\payment::class, rand(6, 25))->create(['account_id' => $v->id])->each(function ($k) {
+        factory(App\Models\balance::class, rand(15, 45))->create(['account_id' => $k->account_id,
+                                                                  'payment_id' => $k->id]);
+      });
+    });
+  }
 }
