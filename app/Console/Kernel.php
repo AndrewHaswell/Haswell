9b9c7d4d18f31f16e3c73a1df7804de649eee4c7
Andrew Haswell <andrew.haswell@visualsoft.co.uk>
2017-04-19 14:59:34 +0100

    Composer update 2

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,6 @@ class Kernel extends ConsoleKernel
    */
   protected function schedule(Schedule $schedule)
   {
-    $schedule->command('payments:update')->everyFiveMinutes();
+    $schedule->command('payments:update')->everyFiveMinutes()->sendOutputTo('payment_update.log');
   }
 }
