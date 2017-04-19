5cb81cbad3e129876c7088f3f4ca9175dc3caa65
Andrew Haswell <andrew.haswell@visualsoft.co.uk>
2017-04-19 19:46:14 +0100

    Added content to home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests;
+use App\Models\Account;
+use App\Models\Schedule;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,6 +27,14 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+      $limit = 15;
+      $accounts = Account::all();
+      $account_list = [];
+      foreach ($accounts as $account) {
+        $account_list[$account->id] = $account->name;
+      }
+      $schedules = Schedule::where('payment_date', '>', Carbon::parse('today'))->orderBy('payment_date', 'asc')->orderBy('type', 'desc')->limit($limit)->get();
+
+        return view('home', compact(['schedules','account_list','limit']));
     }
 }
--- resources/views/payments/payments.blade.php
+++ resources/views/payments/payments.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.app')
 @section('content')
+  <?php
+  $first_letter = 'z';
+  ?>
   <div class="container">
     <div class="row">
       <div class="col-md-10 col-md-offset-1">
@@ -17,9 +20,24 @@
           </thead>
           <tbody>
           @foreach ($payments as $payment)
+
+            <?php
+            $this_first_letter = strtoupper(substr($payment->name, 0, 1));
+            ?>
+
+            @if ($this_first_letter != $first_letter)
+              <tr>
+                <td colspan="6"><h2>{{$this_first_letter}}</h2></td>
+              </tr>
+            @endif
+
+            <?php
+            $first_letter = $this_first_letter;
+            ?>
+
             <tr>
               <th><a href="/payments/{{$payment->id}}">{{ $payment->name }}</a></th>
-              <td>{{ date('j M y',strtotime($payment->start_date)) }}</td>
+              <td>{{ date('j M',strtotime($payment->start_date)) }}</td>
               <td>{{ ucwords($payment->interval) }}</td>
               <?php if ($payment->end_date != '') { ?>
               <td>{{ date('j M y',strtotime($payment->end_date)) }}</td>
