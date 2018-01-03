7c9d41326d53ae672b96ed28f4f52f8db8b64d2c
Andrew Haswell <andrew.haswell@visualsoft.co.uk>
2018-01-03 13:08:42 +0000

    Linked transferred transactions

---
+++ database/migrations/2018_01_03_104405_create_transfers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransfersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transfers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('from_id');
+            $table->integer('to_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('transfers');
+    }
+}
