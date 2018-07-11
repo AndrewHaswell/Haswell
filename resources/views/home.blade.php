8b66e058a3213f5e955fca0cf22441567b129c0e
Andrew Haswell <andrew.haswell@visualsoft.co.uk>
2018-07-11 17:38:01 +0100

    Added Google Pie Chart

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,7 +28,9 @@
 
             <tr>
               <td>{{$schedule->name}}</td>
-              <td><?= !empty($schedule->category_id) && !empty($category_list[$schedule->category_id]) ? $category_list[$schedule->category_id]:'-'?></td>
+              <td><?= !empty($schedule->category_id) && !empty($category_list[$schedule->category_id]) ?
+                  $category_list[$schedule->category_id] :
+                  '-'?></td>
               <td>{{date('D jS F Y',strtotime($schedule->payment_date))}}</td>
               <td align="right">{{number_format($schedule->amount, 2, '.',',')}}</td>
               <td>{{$account_list[$schedule->account_id]}}</td>
@@ -52,19 +54,31 @@
           </thead>
 
           <tbody>
+          <?php
+          $by_category = [];
+          $category_list[0] = 'Other';
+          ?>
           @foreach ($transactions as $transaction)
 
             <?php
-            if ($transaction->type == 'debit')
+            if ($transaction->type == 'debit') {
+              if (!empty($by_category[$category_list[$transaction->category_id]])) {
+                $by_category[$category_list[$transaction->category_id]] += (float)$transaction->amount;
+              } else {
+                $by_category[$category_list[$transaction->category_id]] = (float)$transaction->amount;
+              }
               $transaction->amount = $transaction->amount * -1;
+            }
             ?>
 
             <tr>
               <td><a
-                    href="/transactions/{{$transaction->id}}">{{ $transaction->name }}</a><?= !$transaction->confirmed
-                  ? '&nbsp;&nbsp;&nbsp;<img src="http://www.rccanada.ca/rccforum/images/rccskin/misc/cross.png"/>'
-                  : ''?></td>
-              <td><?= !empty($transaction->category_id) && !empty($category_list[$transaction->category_id]) ? $category_list[$transaction->category_id]:'-'?></td>
+                    href="/transactions/{{$transaction->id}}">{{ $transaction->name }}</a><?= !$transaction->confirmed ?
+                  '&nbsp;&nbsp;&nbsp;<img src="http://www.rccanada.ca/rccforum/images/rccskin/misc/cross.png"/>' :
+                  ''?></td>
+              <td><?= !empty($transaction->category_id) && !empty($category_list[$transaction->category_id]) ?
+                  $category_list[$transaction->category_id] :
+                  '-'?></td>
               <td>{{date('D jS F Y',strtotime($transaction->payment_date))}}</td>
               <td align="right">{{number_format($transaction->amount, 2, '.',',')}}</td>
               <td>{{$account_list[$transaction->account_id]}}</td>
@@ -74,9 +88,38 @@
           @endforeach
           </tbody>
         </table>
-
+        <div id="piechart" style="width: 100%; height: 500px;"></div>
       </div>
     </div>
   </div>
+
+  <script type="text/javascript">
+    google.charts.load('current', {'packages': ['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Category');
+      data.addColumn('number', 'Amount');
+      data.addRows([
+        <?php
+        foreach ($by_category as $key => $value) {
+          echo "['" . $key . "'," . $value . "],";
+        }
+        ?>
+      ]);
+      var formatter = new google.visualization.NumberFormat({
+        prefix: '£'
+      });
+      formatter.format(data, 1); // Apply formatter to second column
+      var options = {
+        pieSliceText: 'label',
+        title: 'Last {{$limit}} Days',
+        is3D: true,
+      };
+      var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+      chart.draw(data, options);
+    }
+  </script>
 
 @endsection
